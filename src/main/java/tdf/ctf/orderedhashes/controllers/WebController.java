b5be9cb9743e57d92c136e8d8350c7cbfd24ef44
Maxime DELIS <md.tech.pro@proton.me>
2025-05-09 17:10:27 +0200

    dep cleanup

--- src/main/java/tdf/ctf/orderedhashes/controllers/WebController.java
+++ src/main/java/tdf/ctf/orderedhashes/controllers/WebController.java
@@ -13,7 +13,6 @@ import org.springframework.security.web.context.HttpSessionSecurityContextReposi
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.validation.BindingResult;
-import org.springframework.validation.ObjectError;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,7 +21,6 @@ import tdf.ctf.orderedhashes.models.LoginForm;
 import tdf.ctf.orderedhashes.models.PasswordChangeForm;
 import tdf.ctf.orderedhashes.models.RegistrationForm;
 import tdf.ctf.orderedhashes.models.User;
-import tdf.ctf.orderedhashes.repositories.UserRepository;
 import tdf.ctf.orderedhashes.services.UserService;
 
 import java.nio.charset.StandardCharsets;
